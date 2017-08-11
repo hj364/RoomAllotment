55373d80897723a68f2aa568ccb14306d788205b
Harshwardhan Jain <hj364@cornell.edu>
2017-08-11 15:00:28 -0400

    finished class Room

--- RoomAllotment.py
+++ RoomAllotment.py
@@ -4,25 +4,40 @@ import sys
 class Bidder:
 
 	def __init__(self, name):
-		self.name = name
+		self._name = name
+		self._hasCleared = False;
+		self._room_num = None
 
 	def get_name(self):
 		return self.name
 
+	def match(self, room_num):
+		self.hasCleared = True
+		self.room_num = room_num
 
+class Room:
+
+	def __init__(self, number, rent):
+		self._number = number
+		self._rent = rent
+
+	def increment(self, amount):
+		self._rent += amount
+
+	def get_rent(self):
+		return self._rent
 
 class Apartment:
 
 	def __init__(self, num_rooms, rent, increment = 5):
 		self.rooms = range(num_rooms)
 		self.rent = range(num_rooms)
-		self.numBids = range(num_rooms)
 		self.increment = increment
 
 		for i in range(num_rooms):
 			self.rooms[i] += 1
 			self.rent[i] = rent
-			self.numBids[i] = 0
+
 
 
 	def updatePrices(self, bids):
@@ -48,9 +63,10 @@ if  __name__ == "__main__":
 	while i < num_rooms
 		try:
 			occupant = raw_input("Enter the name of bidder " + str(i+1) + ".")
-			occupants[i] = occupant
+			occupants[i] = Bidder(occupant)
 			i+=1
 		except TypeErrror:
-			print "Only names in english are accepted. Please type in the correct name."
+			print "Only strings are accepted. Please type in the correct name."
 
-	
+
+
